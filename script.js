0e75973f829cc73c68a30493f78b23fbee6a5773
metasheet <151730162+metasheet@users.noreply.github.com>
2023-11-23 14:09:55 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
-function downloadVideo() {
+function openVideo() {
     var videoUrl = document.getElementById('videoUrl').value;
-    var videoId = getVideoId(videoUrl);
-
-    if (videoId) {
-        var downloadLink = 'https://www.youtubepp.com/watch?v=' + videoId;
-        window.location.href = downloadLink;
-    } else {
-        alert('Invalid YouTube video link. Please check the link and try again.');
-    }
-}
-
-function getVideoId(url) {
-    // Extract video ID from various YouTube video URL formats
-    var regex = /[?&]v=([^?&]+)/;
-    var match = url.match(regex);
-
-    return match && match[1] ? match[1] : null;
+    window.open(videoUrl, '_blank');
 }
